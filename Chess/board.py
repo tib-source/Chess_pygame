77c9ded7a6be8d94b9a44829b35ef6b6945676c0
Tsquare25 <tibebe1234t@gmail.com>
2021-05-26 19:52:13 +0100

    Switched the positions of the peices - white in bottom , black on top

--- Chess/board.py
+++ Chess/board.py
@@ -22,14 +22,14 @@ class Board:
 
     def create_board(self,screen):
         #Black Peices  
-        Rook_0_Black =   Rook( self.gameBoard[-1][0][0] , self.gameBoard[-1][0][1] , "Black" )        
-        Bishop_0_Black = Bishop( self.gameBoard[-1][1][0] , self.gameBoard[-1][1][1] , "Black" )        
-        Knight_0_Black = Knight( self.gameBoard[-1][2][0] , self.gameBoard[-1][2][1] , "Black" )        
-        King_0_Black =   King( self.gameBoard[-1][3][0] , self.gameBoard[-1][3][1] , "Black" )        
-        Queen_0_Black =  Queen( self.gameBoard[-1][4][0] , self.gameBoard[-1][4][1] , "Black" )        
-        Knight_1_Black = Knight( self.gameBoard[-1][5][0] , self.gameBoard[-1][5][1] , "Black" )        
-        Bishop_1_Black = Bishop( self.gameBoard[-1][6][0] , self.gameBoard[-1][6][1] , "Black" )        
-        Rook_1_Black =   Rook( self.gameBoard[-1][7][0] , self.gameBoard[-1][7][1] , "Black" )
+        Rook_0_Black =   Rook( self.gameBoard[-8][0][0] ,  self.gameBoard[-8][0][1] ,  "Black" )        
+        Bishop_0_Black = Bishop( self.gameBoard[-8][1][0] ,  self.gameBoard[-8][1][1] ,  "Black" )        
+        Knight_0_Black = Knight( self.gameBoard[-8][2][0] ,  self.gameBoard[-8][2][1] ,  "Black" )        
+        King_0_Black =   King( self.gameBoard[-8][3][0] ,  self.gameBoard[-8][3][1] ,  "Black" )        
+        Queen_0_Black =  Queen( self.gameBoard[-8][4][0] ,  self.gameBoard[-8][4][1] ,  "Black" )        
+        Knight_1_Black = Knight( self.gameBoard[-8][5][0] ,  self.gameBoard[-8][5][1] ,  "Black" )        
+        Bishop_1_Black = Bishop( self.gameBoard[-8][6][0] ,  self.gameBoard[-8][6][1] ,  "Black" )        
+        Rook_1_Black =   Rook( self.gameBoard[-8][7][0] ,  self.gameBoard[-8][7][1] ,  "Black" )
 
         wh_list = [
             Rook_0_Black,
@@ -44,24 +44,24 @@ class Board:
 
         for i, x in enumerate(wh_list):
             x.draw(x.color, screen)
-            self.board[0].append( Spot( 0*SQUARE, i*SQUARE, x ))
+            self.board[7].append( Spot( 0*SQUARE, i*SQUARE, x ))
 
         Pawns = {}
         for i in range(0, BOARD_LENGTH):
-            Pawns["p "+str(i)+" b"] = Pawn_ = Pawn( self.gameBoard[6][i][0], self.gameBoard[6][i][1], "Black")
+            Pawns["p_"+str(i)+"_b"] = Pawn_ = Pawn( self.gameBoard[1][i][0], self.gameBoard[1][i][1], "Black")
             Pawn_.draw(Pawn_.color, screen)
-            self.board[1].append( Spot( 6*SQUARE, i*SQUARE, Pawn_ ))
+            self.board[6].append( Spot( 1*SQUARE, i*SQUARE, Pawn_ ))
 
 
         #White Peices  
-        Rook_0_White =   Rook( self.gameBoard[0][0][0] , self.gameBoard[0][0][1] , "White")
-        Bishop_0_White = Bishop( self.gameBoard[0][1][0] , self.gameBoard[0][1][1] , "White")    
-        Knight_0_White = Knight( self.gameBoard[0][2][0] , self.gameBoard[0][2][1] , "White")     
-        King_0_White =   King( self.gameBoard[0][3][0] , self.gameBoard[0][3][1] , "White")    
-        Queen_0_White =  Queen( self.gameBoard[0][4][0] , self.gameBoard[0][4][1] , "White")     
-        Knight_1_White = Knight( self.gameBoard[0][5][0] , self.gameBoard[0][5][1] , "White")      
-        Bishop_1_White = Bishop( self.gameBoard[0][6][0] , self.gameBoard[0][6][1] , "White")      
-        Rook_1_White =   Rook( self.gameBoard[0][7][0] , self.gameBoard[0][7][1] , "White")
+        Rook_0_White =   Rook( self.gameBoard[7][0][0] ,  self.gameBoard[7][0][1] ,  "White")
+        Bishop_0_White = Bishop( self.gameBoard[7][1][0] ,  self.gameBoard[7][1][1] ,  "White")    
+        Knight_0_White = Knight( self.gameBoard[7][2][0] ,  self.gameBoard[7][2][1] ,  "White")     
+        King_0_White =   King( self.gameBoard[7][3][0] ,  self.gameBoard[7][3][1] ,  "White")    
+        Queen_0_White =  Queen( self.gameBoard[7][4][0] ,  self.gameBoard[7][4][1] ,  "White")     
+        Knight_1_White = Knight( self.gameBoard[7][5][0] ,  self.gameBoard[7][5][1] ,  "White")      
+        Bishop_1_White = Bishop( self.gameBoard[7][6][0] ,  self.gameBoard[7][6][1] ,  "White")      
+        Rook_1_White =   Rook( self.gameBoard[7][7][0] ,  self.gameBoard[7][7][1] ,  "White")
 
         wh_list = [
             Rook_0_White,
@@ -80,59 +80,7 @@ class Board:
 
         
         for i in range(0, BOARD_LENGTH):
-            Pawns["p "+str(i)+" w"] = Pawn_ = Pawn( self.gameBoard[1][i][0], self.gameBoard[1][i][1], "White")
+            Pawns["p_"+str(i)+"_w"] = Pawn_ = Pawn( self.gameBoard[-2][i][0], self.gameBoard[-2][i][1], "White")
             Pawn_.draw(Pawn_.color, screen)
             self.board[1].append( Spot( 1*SQUARE, i*SQUARE, Pawn_ ))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # def resetBoard(self):
-    #     #TODO: this assignement is fucked, I want it so that i can acces the Spot objects
-    #     #TODO: find a way to store these Objects.
-    #     #TODO: i need them for calculating moves and etc in the Game Class 
-    #     #white 
-    #     Spot( self.gameBoard[7][0] , self.gameBoard[0][1] , Rook.setWhite())
-    #     Spot( self.gameBoard[7][0] , self.gameBoard[1][1] , Knight.setWhite())
-    #     Spot( self.gameBoard[7][0] , self.gameBoard[2][1] , Bishop.setWhite())
-    #     Spot( self.gameBoard[6][0] , self.gameBoard[0][1] , Pawn.setWhite())
-    #     Spot( self.gameBoard[6][0] , self.gameBoard[1][1] , Pawn.setWhite())
-    #     Spot( self.gameBoard[6][0] , self.gameBoard[2][1] , Pawn.setWhite())
-
-
-    #     #black
-    #     Spot( self.gameBoard[0][0] , self.gameBoard[0][1] , Rook)
-    #     Spot( self.gameBoard[0][0] , self.gameBoard[1][1] , Knight)
-    #     Spot( self.gameBoard[0][0] , self.gameBoard[2][1] , Bishop)
-    #     Spot( self.gameBoard[1][0] , self.gameBoard[0][1] , Pawn)
-    #     Spot( self.gameBoard[1][0] , self.gameBoard[1][1] , Pawn)
-    #     Spot( self.gameBoard[1][0] , self.gameBoard[2][1] , Pawn)
-    #     pass
-        
-    #     for i in range(2,6):
-    #         for j in range(0, 8):
-    #             Spot(self.gameBoard[j][0], self.gameBoard[i][1], None)
