470af531d68db70f5e418de625bdf73c983dac02
Tsquare25 <tibebe1234t@gmail.com>
2021-05-26 11:01:51 +0100

    Created the main pygame loop

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 import pygame
 import sys
+from abc import ABC, abstractmethod
+from Chess.constants import *
 
-pygame.init()
+# Game Logic
 
-board = [[" "]*8 for x in range(8)]
-size = width, height = 800, 600
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-SQUARE = 20
+"""
+Currently im thinking of not making a function that moves the peices inside the object class of the peice.
+Instead, ill create an object for every single spot and for them give them a setPiece fucntion>
+later on i can check a move of a peice with the "canMove" and the use the end.setPiece to complete it 
 
-screen = pygame.display.set_mode(size)
+"""
+
+
+
+
+
+class Move:
+    def __init__(self, Player, start, end) -> None:
+        self.Player = Player
+        self.start = start 
+        self.end = end 
+        self.PieceMoved = start.Piece
+        self.castilingMove = False
+
+    #TODO : FINISH THIS CLASS
+
+
+# Pygame GUI 
+def main():
+    #initialise the pygame module here   
+    pygame.init()
+
+    size = (WIDTH,HEIGHT)
+    screen = pygame.display.set_mode(size)
+    screen.fill(BLACK)
+
+
+
+    pygame.display.flip()
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.update()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
